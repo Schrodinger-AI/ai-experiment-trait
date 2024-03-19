cda9ad3dfd6914bc7d836b63239b7538a7f9dc81
Iwaymen <iwaymen@schrodingernft.ai>
2024-03-19 13:21:16 +0800

    Removed idle timer feature

--- src/view/routes/appRoutes.jsx
+++ src/view/routes/appRoutes.jsx
@@ -25,7 +25,6 @@ import ExperimentDetailsPage from '../pages/experimentDetails';
 import ImageGeneratorPage from '../pages/imageGenerator';
 import HelpPage from '../pages/help';
 import Auth from '../pages/credential/auth';
-import IdleTimer from '../pages/credential/idleTimer';
 
 // STYLE IMPORT
 import useStyles from './styles';
@@ -55,7 +54,6 @@ const AppRoutes = () => {
                 </Box>
             </Box>
             <Auth/>
-            <IdleTimer/>
         </Box>
     );
 };
